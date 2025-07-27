d35b7a8d5054272ccbd28b14bb0c300b9eace815
samsharma777 <samsharma202247@gmail.com>
2025-07-27 19:55:18 -0400

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+// src/main/java/com/example/App.java
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("🚀 Hello from Dockerized Java App!");
+    }
+}
